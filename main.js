d47d9afb95f47b578785864c4d1f79d9e4bfe570
MercifulB <112597893+MercifulB@users.noreply.github.com>
2023-02-17 13:19:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-import { AmbientLight, Int8BufferAttribute, SphereGeometry } from 'three';
-
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
